26253275cdefb3c2e310085220190e448d426a16
gmazur92 <gmazur92@gmail.com>
2021-05-15 13:59:17 +0300

    refactor: change structure of the user module

---
+++ src/resources/users/user.controller.js
@@ -0,0 +1,27 @@
+const User = require('./user.model');
+const usersService = require('./user.service');
+
+const getAll = async(req, res) => {
+  const users = await usersService.getAll();
+  res.json(users.map(User.toResponse));
+};
+const get = async(req, res) => {
+  const user = await usersService.get(req.params.id);
+  res.json(User.toResponse(user));
+};
+const create = async(req, res) => {
+  const newUser = await usersService.create(req.body);
+  res.status(201).json(User.toResponse(newUser));
+};
+const update = async(req, res) => {
+  const updatedUser = await usersService.update(req.params.id, req.body);
+  res.status(200).json(User.toResponse(updatedUser));
+};
+const deleteById = async(req, res) => {
+  await usersService.deleteUser(req.params.id);
+  res.status(204).json({});
+};
+
+module.exports = {
+  getAll, get, create, update, deleteById
+};
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,30 +1,10 @@
 const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const userController = require('./user.controller')
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  res.json(users.map(User.toResponse));
-});
-
-router.route('/:id').get(async (req, res) => {
-  const user = await usersService.get(req.params.id);
-  res.json(User.toResponse(user));
-});
-
-router.route('/').post(async (req, res) => {
-  const newUser = await usersService.create(req.body);
-  res.status(201).json(User.toResponse(newUser));
-});
-
-router.route('/:id').put(async (req, res) => {
-  const updatedUser = await usersService.update(req.params.id, req.body);
-  res.status(200).json(User.toResponse(updatedUser));
-});
-
-router.route('/:id').delete(async (req, res) => {
-  await usersService.deleteUser(req.params.id);
-  res.status(204).json({});
-});
+router.get('/', userController.getAll )
+router.get('/:id', userController.get )
+router.post('/', userController.create )
+router.put('/:id', userController.update )
+router.delete('/:id', userController.deleteById )
 
 module.exports = router;
