68ea726d266d617023c2d9cd2dd7d47ee7bb1427
gmazur92 <gmazur92@gmail.com>
2021-05-15 20:43:38 +0300

    refactor: change functions to class methods in board and task controllers

--- src/resources/boards/board.controller.js
+++ src/resources/boards/board.controller.js
@@ -1,38 +1,36 @@
 const Board = require('./board.model');
 const boardsService = require('./board.service');
 
-const getAll = async(req, res) => {
-  const boards = await boardsService.getAll();
-  res.json(boards.map(Board.toResponse));
-};
+module.exports = class BoardController {
+  static async getAll(req, res) {
+    const boards = await boardsService.getAll();
+    res.json(boards.map(Board.toResponse));
+  };
 
-const get = async(req, res) => {
-  const board = await boardsService.get(req.params.id);
-  if (!board) {
-    res.status(404).json({});
-  }
-  res.json(Board.toResponse(board));
-};
+  static async get(req, res) {
+    const board = await boardsService.get(req.params.id);
+    if (!board) {
+      res.status(404).json({});
+    }
+    res.json(Board.toResponse(board));
+  };
 
-const create = async(req, res) => {
-  const newBoard = await boardsService.create(req.body);
-  res.status(201).json(Board.toResponse(newBoard));
-};
+  static async create(req, res) {
+    const newBoard = await boardsService.create(req.body);
+    res.status(201).json(Board.toResponse(newBoard));
+  };
 
-const update = async(req, res) => {
-  const updatedBoard = await boardsService.update(req.params.id, req.body);
-  res.status(200).json(Board.toResponse(updatedBoard));
-};
+  static async update(req, res) {
+    const updatedBoard = await boardsService.update(req.params.id, req.body);
+    res.status(200).json(Board.toResponse(updatedBoard));
+  };
 
-const deleteById = async(req, res) => {
-  try {
-    await boardsService.deleteBoard(req.params.id);
-    res.status(204).json({});
-  } catch (e) {
-    res.status(404).json({});
-  }
-};
-
-module.exports = {
-  getAll, get, create, update, deleteById
+  static async deleteById(req, res) {
+    try {
+      await boardsService.deleteBoard(req.params.id);
+      res.status(204).json({});
+    } catch (e) {
+      res.status(404).json({});
+    }
+  };
 };
--- src/resources/tasks/task.controller.js
+++ src/resources/tasks/task.controller.js
@@ -1,35 +1,33 @@
 const Task = require('./task.model');
 const taskService = require('./task.service');
 
-const getAll = async(req, res) => {
-  const tasks = await taskService.getAll();
-  res.json(tasks.map(Task.toResponse));
-};
+module.exports = class TaskController {
+  static async getAll(req, res) {
+    const tasks = await taskService.getAll();
+    res.json(tasks.map(Task.toResponse));
+  };
 
-const get = async(req, res) => {
-  const task = await taskService.get(req.params.id);
-  if (!task) {
-    res.status(404).json({});
-  }
-  res.json(task);
-};
+  static async get(req, res) {
+    const task = await taskService.get(req.params.id);
+    if (!task) {
+      res.status(404).json({});
+    }
+    res.json(task);
+  };
 
-const create = async(req, res) => {
-  const newTask = {...req.body, boardId: req.params.boardId}
-  const createdTask = await taskService.create(req.params.id, newTask);
-  res.status(201).json(Task.toResponse(createdTask));
-};
+  static async create(req, res) {
+    const newTask = {...req.body, boardId: req.params.boardId}
+    const createdTask = await taskService.create(req.params.id, newTask);
+    res.status(201).json(Task.toResponse(createdTask));
+  };
 
-const update = async(req, res) => {
-  const updatedTask = await taskService.update(req.params.id, req.body);
-  res.status(200).json(Task.toResponse(updatedTask));
-};
+  static async update(req, res) {
+    const updatedTask = await taskService.update(req.params.id, req.body);
+    res.status(200).json(Task.toResponse(updatedTask));
+  };
 
-const deleteById = async(req, res) => {
-  await taskService.deleteTask(req.params.id);
-  res.status(204).json({});
-};
-
-module.exports = {
-  getAll, get, create, update, deleteById
+  static async deleteById(req, res) {
+    await taskService.deleteTask(req.params.id);
+    res.status(204).json({});
+  };
 };
