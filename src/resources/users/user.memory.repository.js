0036d13984a7c7e29cc4c9986ba020e7a019d102
gmazur92 <gmazur92@gmail.com>
2021-05-26 00:13:54 +0300

    feat: add jsdoc description to user repository and service modules

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -2,36 +2,36 @@ const db = require('../../common/db');
 const {USERS} = require('../../common/constants');
 /**
  * Create a new user in db
- * @param {User} body
- * @returns {Promise<User>}
+ * @param {User} body object with user data
+ * @returns {Promise<User>}  returns promise with created user
  */
 const create = async(body) => db.create(USERS, body);
 
 /**
  * Get all users from db
- * @returns {Promise<Array<User>>}
+ * @returns {Promise<Array<User>>} returns promise with array of users
  */
 const getAll = async() => db.getAll(USERS);
 
 /**
  * Get a single user from db
- * @param {string} id
- * @returns {Promise<User>}
+ * @param {string} id  user id
+ * @returns {Promise<User>} returns promise with single user
  */
 const get = async(id) => db.get(USERS, id);
 
 /**
  * Update a user in db
- * @param {string} id
- * @param {User} body
- * @returns {Promise<User>}
+ * @param {string} id user id
+ * @param {User} body object with data
+ * @returns {Promise<User>}  returns promise with updated user
  */
 const update = async(id, body) => db.update(USERS, id, body);
 
 /**
  * Delete user from db
- * @param {string} id
- * @returns {Promise<{}>}
+ * @param {string} id user id
+ * @returns {Promise<{}>}  returns promise with empty object if user deleted
  */
 const deleteUser = async(id) => db.deleteById(USERS, id);
 
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -3,37 +3,37 @@ const tasksService = require('../tasks/task.service')
 
 /**
  * Get all users from db
- * @returns {Promise<Array<User>>}
+ * @returns {Promise<Array<User>>} returns promise with array of users
  */
 const getAll = () => usersRepo.getAll();
 
 /**
  * Get a single user from db
- * @param {string} id
- * @returns {Promise<User>}
+ * @param {string} id  user id
+ * @returns {Promise<User>} returns promise with single user
  */
 const get = (id) => usersRepo.get(id);
 
 /**
  * Create a new user in db
- * @param {User} body
- * @returns {Promise<User>}
+ * @param {User} body object with user data
+ * @returns {Promise<User>}  returns promise with created user
  */
 const create = (body) => usersRepo.create(body);
 
 /**
  * Update a user in db
- * @param {string} id
- * @param {User} body
- * @returns {Promise<User>}
+ * @param {string} id user id
+ * @param {User} body object with data
+ * @returns {Promise<User>}  returns promise with updated user
  */
 const update = (id, body) => usersRepo.update(id, body);
 
 /**
  * Delete user from db
  * @async
- * @param {string} id
- * @returns {Promise<{}>}
+ * @param {string} id user id
+ * @returns {Promise<{}>}  returns promise with empty object if user deleted
  */
 const deleteUser = async (id) => {
   await usersRepo.deleteUser(id);
