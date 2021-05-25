b97a68cd19dae64bda2cff27b65ce434c29f95d3
gmazur92 <gmazur92@gmail.com>
2021-05-25 23:27:44 +0300

    feat: add params description in board repository and board service

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -3,36 +3,36 @@ const {BOARDS} = require('../../common/constants');
 
 /**
  * Request to db to create a new board
- * @param {Board} body
- * @returns {Promise<Board>}
+ * @param {Board} body board to be created
+ * @returns {Promise<Board>} returns promise of created board
  */
 const create = async(body) => db.create(BOARDS, body);
 
 /**
  * Get all boards from db
- * @returns {Promise<Array<Board>>}
+ * @returns {Promise<Array<Board>>} returns promise with array of all boards
  */
 const getAll = async() => db.getAll(BOARDS);
 
 /**
  * Get single board from db
- * @param {string} id
- * @returns {Promise<Board>}
+ * @param {string} id id of single board
+ * @returns {Promise<Board>} returns promise with single board
  */
 const get = async(id) => db.get(BOARDS, id);
 
 /**
  * Update board
- * @param {string} id
- * @param {Board} body
- * @returns {Promise<Board>}
+ * @param {string} id id of board to be updated
+ * @param {Board} body object with data to update
+ * @returns {Promise<Board>} returns updated board
  */
 const update = async(id, body) => db.update(BOARDS, id, body);
 
 /**
  * Delete board from db
- * @param {string} id
- * @returns {Promise<{}>}
+ * @param {string} id id of board to be deleted
+ * @returns {Promise<{}>} returns an empty object if board deleted
  */
 const deleteBoard = async(id) => db.deleteById(BOARDS, id);
 
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -3,37 +3,37 @@ const tasksService = require('../tasks/task.service');
 
 /**
  * Get all boards from db
- * @returns {Promise<Array<Board>>}
+ * @returns {Promise<Array<Board>>} returns promise with array of all boards
  */
 const getAll = () => boardsRepo.getAll();
 
 /**
  * Get single board from db
- * @param {string} id
- * @returns {Promise<Board>}
+ * @param {string} id id of single board
+ * @returns {Promise<Board>} returns promise with single board
  */
 const get = (id) => boardsRepo.get(id);
 
 /**
  * Request to db to create a new board
- * @param {Board} body
- * @returns {Promise<Board>}
+ * @param {Board} body board to be created
+ * @returns {Promise<Board>} returns promise of created board
  */
 const create = (body) => boardsRepo.create(body);
 
 /**
  * Update board
- * @param {string} id
- * @param {Board} body
- * @returns {Promise<Board>}
+ * @param {string} id id of board to be updated
+ * @param {Board} body object with data to update
+ * @returns {Promise<Board>} returns updated board
  */
 const update = (id, body) => boardsRepo.update(id, body);
 
 /**
  * Delete board from db
  * @async
- * @param {string} id
- * @returns {Promise<{}>}
+ * @param {string} id id of board to be deleted
+ * @returns {Promise<{}>} returns an empty object if board deleted
  */
 const deleteBoard = async(id) => {
   await boardsRepo.deleteBoard(id);
