33661983ebecf6199c846002a4ca14eb1abccda7
gmazur92 <gmazur92@gmail.com>
2021-05-30 21:10:26 +0300

    refactor: change require to import add types in column model

--- src/resources/boards/column.model.ts
+++ src/resources/boards/column.model.ts
@@ -1,34 +1,42 @@
-const {v4: uuidv4} = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IColumn {
+  id: string;
+  title: string;
+  order: number;
+}
+
+export type IColumnProps = Omit<IColumn, 'id'>;
 
 /**
  * Column model
  * This class is used in order to create a new column instance in db
  */
-class Column {
+class Column implements IColumn {
+  id: string = uuidv4();
+
+  title: string;
+
+  order: number;
+
   /**
    * @param {object} params
-   * @param {string} id - column id
    * @param {string} title - column title
    * @param {number} order - order
    */
-  constructor({
-    id = uuidv4(),
-    title = 'COLUMN',
-    order = 1,
-  } = {}) {
+  constructor({ title = 'COLUMN', order = 1 }: Partial<IColumnProps>) {
     this.title = title;
-    this.id = id;
     this.order = order;
   }
 
   /**
-    * Static method returns column as a response to request
+   * Static method returns column as a response to request
    * @param {Column} column
    * @returns {Column}
    */
-  static toResponse(column) {
+  static toResponse(column: IColumn): IColumn {
     return column;
   }
 }
 
-module.exports = Column;
+export default Column;
