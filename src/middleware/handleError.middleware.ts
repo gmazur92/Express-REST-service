f76ddcd31d62492e1d1de6798934f6586758c856
gmazur92 <gmazur92@gmail.com>
2021-06-06 20:43:58 +0300

    refactor: change handleError middleware to arrow functions

--- src/middleware/handleError.middleware.ts
+++ src/middleware/handleError.middleware.ts
@@ -4,7 +4,7 @@ import { BaseError } from '../error/BaseError';
 import { logger } from '../logger/logger';
 
 class ErrorHandler {
-  public async handleError(err: BaseError, _req: Request, res: Response, _next: NextFunction): Promise<void> {
+  public handleError = async(err: BaseError, _req: Request, res: Response, _next: NextFunction): Promise<void> => {
     const isErrorSafeForClient = this.isTrustedError(err);
     const clientError = isErrorSafeForClient
       ? {
@@ -13,23 +13,23 @@ class ErrorHandler {
         data: err.stack,
       }
       : {
-      code: 'Internal error',
-      message: 'Something went wrong',
-      status: StatusCodes.INTERNAL_SERVER_ERROR,
-      data: {}
-    };
+        code: 'Internal error',
+        message: 'Something went wrong',
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+        data: {},
+      };
 
     await logger.error('', clientError);
     clientError.data = {};
     res.status(clientError.status).send({error: clientError});
-  }
+  };
 
-  public isTrustedError(error: Error) {
+  public isTrustedError = (error: Error) => {
     if (error instanceof BaseError) {
       return error.isOperational;
     }
     return false;
-  }
+  };
 }
 
 export const errorHandler = new ErrorHandler();
