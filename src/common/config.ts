ef0d0ea17b34a39482b8d7cadd435311aa01b1f1
gmazur92 <gmazur92@gmail.com>
2021-06-20 20:06:08 +0300

    feat: add vars in config

--- src/common/config.ts
+++ src/common/config.ts
@@ -8,6 +8,9 @@ const { AUTH_MODE: authMode } = process.env;
 
 export const { PORT } = process.env;
 export const { NODE_ENV } = process.env;
-export const { MONGO_CONNECTION_STRING } = process.env;
-export const { JWT_SECRET_KEY } = process.env;
+export const { POSTGRES_PORT } = process.env;
+export const { POSTGRES_USER } = process.env;
+export const { POSTGRES_PASSWORD } = process.env;
+export const { POSTGRES_DB } = process.env;
+export const { POSTGRES_HOST } = process.env;
 export const AUTH_MODE = authMode === 'true';
