321dd140a67d6ed8e6b323e9b11c455fe6e9d347
gmazur92 <gmazur92@gmail.com>
2021-06-20 15:23:07 +0300

    refactor: user module

--- src/resources/users/user.controller.ts
+++ src/resources/users/user.controller.ts
@@ -1,64 +1,69 @@
-import express from 'express';
+import { Request, NextFunction, Response } from 'express';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
-import User, { IUser } from './user.model';
-import * as usersService from './user.service';
+import { IUserProps } from './dto/requestUser.dto';
+import * as userService from './user.service';
+import { UserEntity } from '../../entity/User.entity';
+import { ResponseUserDto } from './dto/responseUser.dto';
 
 class UserController {
-  static async getAll(_req: express.Request, res: express.Response) {
+  static async getAll(_req: Request, res: Response, next: NextFunction) {
     try {
-      const users: IUser[] = await usersService.getAll();
-      return res.json(users.map(User.toResponse));
+      const users: UserEntity[] = await userService.getAll();
+      return res.json(users.map(user => new ResponseUserDto(user)));
     } catch (e) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e.message);
+      return next(e);
     }
   }
 
-  static async get(req: express.Request, res: express.Response) {
+  static async get(req: Request<{id: string}>, res: Response, next: NextFunction) {
     const {id} = req.params;
     try {
-      const user: IUser = await usersService.get(id!);
+      const user: UserEntity|undefined = await userService.get(id);
       if (!user) {
         return res.status(StatusCodes.NOT_FOUND).json({message: ReasonPhrases.NOT_FOUND});
       }
-      return res.json(User.toResponse(user));
+      return res.json(new ResponseUserDto(user));
     } catch (e) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e.message);
+      return next(e);
     }
   }
 
-  static async create(req: express.Request, res: express.Response) {
+  static async create(req: Request<IUserProps>, res: Response, next: NextFunction) {
     const {body} = req;
     try {
-      const newUser: IUser = await usersService.create(body);
-      return res.status(StatusCodes.CREATED).json(User.toResponse(newUser));
+      const newUser: UserEntity = await userService.create(body);
+      return res.status(StatusCodes.CREATED).json(new ResponseUserDto(newUser));
     } catch (e) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e.message);
+      return next(e);
     }
   }
 
-  static async update(req: express.Request, res: express.Response) {
+  static async update(req: Request<{id: string}, UserEntity>, res: Response, next: NextFunction) {
     const {id} = req.params;
     try {
-      const updatedUser: IUser = await usersService.update(
-        id!,
+      const updatedUser: UserEntity|null = await userService.update(
+        id,
         req.body,
       );
       if (!updatedUser) {
         return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
       }
-      return res.status(StatusCodes.OK).json(User.toResponse(updatedUser));
+      return res.status(StatusCodes.OK).json(new ResponseUserDto(updatedUser));
     } catch (e) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e.message);
+      return next(e);
     }
   }
 
-  static async deleteById(req: express.Request, res: express.Response) {
+  static async deleteById(req: Request<{id: string}>, res: Response, next: NextFunction) {
     const {id} = req.params;
     try {
-      await usersService.deleteUser(id!);
-      return res.status(204).json({});
+      const result: UserEntity|null = await userService.deleteUser(id);
+      if (!result) {
+        return res.status(StatusCodes.NOT_FOUND).json(ReasonPhrases.NOT_FOUND);
+      }
+      return res.status(StatusCodes.NO_CONTENT).json(ReasonPhrases.NO_CONTENT);
     } catch (e) {
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e.message);
+      return next(e);
     }
   }
 }
