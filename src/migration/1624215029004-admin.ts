be209f404b79e2cb9da21617d164bcff4a2ebfa2
gmazur92 <gmazur92@gmail.com>
2021-06-27 17:59:16 +0300

    feat: add admin migration

---
+++ src/migration/1624215029004-admin.ts
@@ -0,0 +1,15 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class Admin1624215029004 implements MigrationInterface {
+  name = 'Admin1624215029004'
+
+  // eslint-disable-next-line class-methods-use-this
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`INSERT INTO public."user" (login, name, password) VALUES ('admin', 'admin', '$2b$10$pbFbzS0pKm714aXF6TM.iuPt3AjZ99cU6K/jTrvfb19k5ihu7FiKq')`);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM public."user" WHERE login='admin'`);
+  }
+}
