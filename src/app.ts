07ee84033d87be8076950eb7f6277204985c571d
gmazur92 <gmazur92@gmail.com>
2021-06-27 18:20:00 +0300

    feat: add middleware and auth router

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,13 @@ import YAML from 'yamljs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import authRouter from './resources/auth/auth.router';
 import { errorHandler } from './middleware/handleError.middleware';
 import { BaseError } from './error/BaseError';
 import { logger } from './logger/logger';
 import { loggerRequestManager } from './middleware/loggerRequestManager.middleware';
 import { HTTP404Error } from './error/NotFoundError';
+import authMiddleware from './middleware/auth.middleware';
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -23,6 +25,11 @@ app.use('/', (req: Request, res: Response, next) => {
   }
   next();
 });
+
+app.use('/login', authRouter);
+
+app.use(authMiddleware);
+
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards/:boardId/tasks', taskRouter);
@@ -31,16 +38,14 @@ app.use('*', (_req: Request, _res: Response) => {
   throw new HTTP404Error();
 });
 
-app.use(async(err: BaseError, req: Request, res: Response, next: NextFunction) => errorHandler.handleError(err, req, res, next));
+app.use(async (err: BaseError, req: Request, res: Response, next: NextFunction) => errorHandler.handleError(err, req, res, next));
 
-process
-  .on('uncaughtException', (error: Error) => {
+process.on('uncaughtException', (error: Error) => {
   logger.error('uncaughtException', {stack: JSON.stringify(error.stack)});
   setTimeout(() => process.exit(1), 1000);
-})
-  .on('unhandledRejection', (error: Error) => {
+}).on('unhandledRejection', (error: Error) => {
   logger.error('unhandledRejection', {stack: JSON.stringify(error.stack)});
   setTimeout(() => process.exit(1), 1000);
-  });
+});
 
 export default app;
